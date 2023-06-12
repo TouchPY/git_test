8acce70a74d10870ceb403bb58c95a6f54223378
Reese <cczjykzs@163.com>
2023-06-12 19:59:06 +0800

    pytest.py second

--- pytest.py
+++ pytest.py
@@ -1,3 +1,6 @@
+#两种方法求斐波那契数列
+
+#method1
 def feibonaqi(n):
     if n==1 or n==2:
         return 1
@@ -6,3 +9,17 @@ def feibonaqi(n):
 fb=feibonaqi(8)
 print(fb)
 #1 1 2 3 5 8 13 21 34 55 ...
+
+#method2
+def fbnq2(n):
+    a, b = 1, 1
+    if n==1 or n==2:
+        return 1
+    else:
+        for i in range(n-2):
+            c=a+b
+            a=b
+            b=c
+        return b
+fb2=fbnq2(6)
+print(fb2)
